2b524f805d65d6f7dc7ae35655c223ffaa91c07f
e <1195455487@qq.com>
2018-09-20 19:04:00 +0800

    m

--- app/Http/Controllers/Web/ShopLoginController.php
+++ app/Http/Controllers/Web/ShopLoginController.php
@@ -29,23 +29,82 @@ class ShopLoginController extends Controller
         if ($request->isMethod('get')){
             return view('default.web.shop.register');
         } else {
+            $shop = $request->input('shop_name','');
+            $company_name = $request->input('company_name','');
+            $attorney_letter_fileImg = $request->input('attorney_letter_fileImg','');
+            $business_license_id = $request->input('business_license_id','');
+            $license_fileImg = $request->input('license_fileImg','');
+            $taxpayer_id = $request->input('taxpayer_id','');
+            $is_self_run = $request->input('is_self_run','0');
+            $user_name = $request->input('user_name','');
+            $password = $request->input('password','');
+            $password_confirmation = $request->input('password_confirmation','');
+            $mobile= $request->input('mobile','');
+            $mobile_code = $request->input('mobile_code','');
+            $protocol = $request->input('protocol','');
 
-            $rules = [
+            if (empty($protocol)){
+                $errorMsg[] = '未同意协议';
+            }
+
+            if (empty($shop)){
+                $errorMsg[] ='店铺名称不能为空';
+            }
+
+            if (empty($company_name)){
+                $errorMsg[] ='企业名称不能为空';
+            }
+
+            if (empty($attorney_letter_fileImg)){
+                $errorMsg[] ='授权委托书电子版不能为空';
+            }
+
+            if (empty($business_license_id)){
+                $errorMsg[] ='营业执照注册号不能为空';
+            }
+            if (empty($license_fileImg)){
+                $errorMsg[] ='营业执照副本电子版不能为空';
+            }
+            if (empty($taxpayer_id)){
+                $errorMsg[] ='纳税人识别号';
+            }
+            if (empty($user_name)){
+                $errorMsg[] ='用户名称不能为空';
+            }
+            if (empty($password)){
+                $errorMsg[] ='密码不能为空';
+            }
+            if ($password!=$password_confirmation){
+                $errorMsg[] = "两次密码不能一样";
+            }
+            if (empty($mobile)){
+                $errorMsg[] ='手机号不能为空';
+            }
+            if (empty($mobile_code)){
+                $errorMsg[] ='店铺名称不能为空';
+            }
+
+            if (!empty($errorMsg))
+            {
+                return $this->error(implode('<br/>,',$errorMsg));
+            }
+            $data = [
                 'shop_name',
                 'company_name',
                 'attorney_letter_fileImg',
                 'business_license_id',
                 'license_fileImg',
                 'taxpayer_id',
-                'user_name'=>'required|unique:shop_user',
-                'password'=>'required|confirmed|min:6',
-                'mobile'=>'required|regex:/^1[34578][0-9]{9}$/',
-                'protocol'=>'Accepted'
+                'is_self_run',
+                'password',
+                'mobile',
+                'mobile_code',
+                'protocol'
             ];
             try{
-                dd($request);
-                $data = $this->validate($request,$rules);
-                dd($data);
+               $re = ShopLoginService::Register($data);
+
+               dd($re);
             }catch (\Exception $e){
                 dd($e->getMessage());
             }
--- resources/views/default/web/shop/register.blade.php
+++ resources/views/default/web/shop/register.blade.php
@@ -27,22 +27,22 @@
 <body>
 <div class="main" >
 <h1>商户注册</h1>
-<form action="/shop/register" method="post">
-    @csrf
-    店铺名称  <input type="text" name="shop_name"><br>
-    企业全称  <input type="text" name="company_name"><br>
-    授权委托书电子版  <input type="file" name="attorney_letter_fileImg"><br>
-    营业执照注册号  <input type="text" name="business_license_id"><br>
-    营业执照副本电子版  <input type="file" name="license_fileImg"><br>
-    纳税人识别号  <input type="text" name="taxpayer_id"><br>
-    是否自营  <input type="text" name="is_self_run"><br>
-    用户姓名   <input type="text" name="contactName" placeholder="请输入负责人姓名"><br>
-    手机号    <input type="text" name="contactPhone" id="mobile"><br>
-    手机验证码  <input type="text" name="mobile_code"><input type="button" onclick="messageCode();" value="获取验证码" id="code" /><br>
-    密　码   <input type="password" name="password"><br>
-    确认密码  <input type="password" name="password_confirmation"><br>
-            <input name="protocol" type="checkbox" value="on"> <a href="javascript:void(0)">《协议》</a>
-    <input type="submit" value="注册">
+<form>
+    店铺名称  <input type="text" name="shop_name" id="shop_name"><br>
+    企业全称  <input type="text" name="company_name" id="company_name"><br>
+    授权委托书电子版  <input type="file" name="attorney_letter_fileImg" id="attorney_letter_fileImg"><br>
+    营业执照注册号  <input type="text" name="business_license_id" id="business_license_id"><br>
+    营业执照副本电子版  <input type="file" name="license_fileImg" id="license_fileImg"><br>
+    纳税人识别号  <input type="text" name="taxpayer_id" id="taxpayer_id"><br>
+    是否自营  <input type="text" name="is_self_run" id="is_self_run"><br>
+    用户姓名   <input type="text" name="name" id="name" placeholder="请输入负责人姓名"><br>
+    手机号    <input type="text" name="mobile" id="mobile"><br>
+    手机验证码  <input type="text" name="mobile_code" id="mobile_code"><input type="button" onclick="messageCode();" value="获取验证码" id="code" /><br>
+    密　码   <input type="password" name="password" id="password"><br>
+    确认密码  <input type="password" name="password_confirmation" id="password_confirmation"><br>
+            <input name="protocol" type="checkbox" value="1" id="protocol" > <a href="javascript:void(0)">《协议》</a>
+    {{--<input type="button" value="注册" onclick="submit()">--}}
+    <a href="javascript:void(0)" onclick="submit()">注册</a>
     <a href="/shop/login.html">登录</a>
 </form>
 </div>
@@ -51,6 +51,7 @@
     var flag = false;
     function messageCode(){
         var mobile = $('#mobile').val();
+        console.log(mobile);
         if(!flag){
             $.ajax({
                 headers:{ 'X-CSRF-TOKEN': $('input[name="_token"]').val()},
@@ -75,12 +76,43 @@
                                 return false;
                             }
                         },1000)
-
                     }
                 }
             })
         }
         return;
+    }
+
+    function submit() {
+            var _shop_name = $('#shop_name').val();
+            var _company_name = $('#company_name').val();
+            var _attorney_letter_fileImg = $('#attorney_letter_fileImg').val();
+            var _business_license_id = $('#business_license_id').val();
+            var _license_fileImg = $('#license_fileImg').val();
+            var _taxpayer_id = $('#taxpayer_id').val();
+            var _is_self_run = $('#is_self_run').val();
+            var _password = $('#password').val();
+            var _mobile = $('#mobile').val();
+            var _mobile_code = $('#mobile_code').val();
+            // if ($('#protocol').checked())
+
+            var _protocol = $('#protocol').checked ? $('#protocol').val():'';
+
+            let _data = {
+                'shop_name':_shop_name,
+                'company_name':_company_name,
+                'attorney_letter_fileImg':_attorney_letter_fileImg,
+                'business_license_id':_business_license_id,
+                'license_fileImg':_license_fileImg,
+                'taxpayer_id':_taxpayer_id,
+                'is_self_run':_is_self_run,
+                'password':_password,
+                'mobile':_mobile,
+                'mobile_code':_mobile_code,
+                'protocol':_protocol
+            };
+            console.log(_data);
+
     }
 </script>
 </html>
