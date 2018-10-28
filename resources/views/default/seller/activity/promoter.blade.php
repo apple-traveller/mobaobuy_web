50e73afe468c7c6e294d9aaec29f3aa1c78ba34d
e <1195455487@qq.com>
2018-10-28 22:44:56 +0800

    activity

--- resources/views/default/seller/activity/promoter.blade.php
+++ resources/views/default/seller/activity/promoter.blade.php
@@ -12,17 +12,16 @@
             <div class="flexilist mt30" id="listDiv">
                 <div class="common-head order-coomon-head">
                     <div class="fl_label">
-                        <a href="/seller/seckill/add"><div class="fbutton"><div class="add" title="申请"><span><i class="icon icon-plus"></i>添加申请</span></div></div></a>
+                        <a href="/seller/activity/addPromoter"><div class="fbutton"><div class="add" title="申请"><span><i class="icon icon-plus"></i>添加申请</span></div></div></a>
                     </div>
                     <div class="refresh">
                         <div class="refresh_tit" title="刷新数据" onclick="javascript:history.go(0)"><i class="icon icon-refresh"></i></div>
-                        {{--<div class="refresh_span">刷新 - 共{{$total}}条记录</div>--}}
+                        <div class="refresh_span">刷新 - 共{{$total}}条记录</div>
                     </div>
-
                     <div class="search">
-                        <form action="/seller/order/list" name="searchForm" >
+                        <form action="/seller/activity/Promoter" name="searchForm" >
                             <div class="input">
-                                <input type="text" name="order_sn" value="" class="text nofocus w180" placeholder="订单编号" autocomplete="off">
+                                <input type="text" name="order_sn" value="" class="text nofocus w180" placeholder="商品名称" autocomplete="off">
                                 <input type="submit" class="btn" name="secrch_btn" ectype="secrch_btn" value="">
                             </div>
                         </form>
@@ -36,36 +35,42 @@
                             <table cellpadding="0" cellspacing="0" border="0">
                                 <thead>
                                 <tr>
-                                    <th width="10%"><div class="tDiv">编号</div></th>
-                                    <th width="10%"><div class="tDiv">申请时间</div></th>
-                                    <th width="10%"><div class="tDiv">促销商品</div></th>
-                                    <th width="10%"><div class="tDiv">促销价格</div></th>
-                                    <th width="10%"><div class="tDiv">促销总数量</div></th>
-                                    <th width="10%"><div class="tDiv">当前可销售数量</div></th>
-                                    <th width="10%"><div class="tDiv">最小起售量</div></th>
-                                    <th width="10%"><div class="tDiv">最大限购量</div></th>
-                                    <th width="10%"><div class="tDiv">活动开始时间</div></th>
-                                    <th width="10%"><div class="tDiv">活动结束时间</div></th>
-                                    <th width="15%"><div class="tDiv">审核状态</div></th>
-                                    <th width="25%"><div class="tDiv">操作</div></th>
+                                    <th><div class="tDiv">编号</div></th>
+                                    <th><div class="tDiv">申请时间</div></th>
+                                    <th><div class="tDiv">促销商品</div></th>
+                                    <th><div class="tDiv">促销价格</div></th>
+                                    <th><div class="tDiv">促销总数量</div></th>
+                                    <th><div class="tDiv">当前可销售数量</div></th>
+                                    <th><div class="tDiv">最小起售量</div></th>
+                                    <th><div class="tDiv">最大限购量</div></th>
+                                    <th><div class="tDiv">开始时间</div></th>
+                                    <th><div class="tDiv">结束时间</div></th>
+                                    <th><div class="tDiv">审核状态</div></th>
+                                    <th><div class="tDiv">操作</div></th>
                                 </tr>
                                 </thead>
                                 <tbody>
-                        {{--        @foreach($list as $k=>$v)
+                                @foreach($list as $k=>$v)
                                     <tr class="">
                                         <td><div class="tDiv">{{$v["id"]}}</div></td>
                                         <td><div class="tDiv">{{$v['add_time']}}</div></td>
+                                        <td><div class="tDiv">{{$v['goods_name']}}</div></td>
+                                        <td><div class="tDiv">{{$v['price']}}</div></td>
+                                        <td><div class="tDiv">{{$v['num']}}</div></td>
+                                        <td><div class="tDiv">{{$v['available_quantity']}}</div></td>
+                                        <td><div class="tDiv">{{$v['min_limit']}}</div></td>
+                                        <td><div class="tDiv">{{$v['max_limit']}}</div></td>
                                         <td><div class="tDiv">{{$v['begin_time']}}</div></td>
                                         <td><div class="tDiv">{{$v['end_time']}}</div></td>
                                         <td><div class="tDiv">@if($v['review_status']==1)待审核 @elseif($v['review_status']==2)审核不通过@elseif($v['review_status']==3)审核通过@endif</div></td>
                                         <td class="handle">
                                             <div class="tDiv a3">
-                                                <a href="/seller/seckill/list_detail?seckill_id={{$v['id']}}"  title="查看" class="btn_see"><i class="sc_icon sc_icon_see"></i>查看</a>
-                                                <a href="javascript:void(0)"  data_id = "{{$v['id']}}" data_page = "{{$currentPage}}" title="删除" class="btn_trash" id="btn_trash"><i class="sc_icon icon-trash"></i>删除</a>
+                                                <a href="/seller/activity/addPromoter?id={{$v['id']}}"  title="编辑" class="btn_see"><i class="sc_icon sc_icon_see"></i>编辑</a>
+                                                <a href="javascript:void(0);"  data_id = "{{$v['id']}}" data_page = "{{$currentPage}}" title="删除" class="btn_trash"><i class="sc_icon icon-trash"></i>删除</a>
                                             </div>
                                         </td>
                                     </tr>
-                                @endforeach--}}
+                                @endforeach
                                 </tbody>
                                 <tfoot>
                                 <tr>
@@ -104,32 +109,32 @@
         function paginate(){
             layui.use(['laypage'], function() {
                 var laypage = layui.laypage;
-                {{--laypage.render({--}}
-                    {{--elem: 'page' //注意，这里 是 ID，不用加 # 号--}}
-                    {{--, count: "{{$total}}" //数据总数，从服务端得到--}}
-                    {{--, limit: "{{$pageSize}}"   //每页显示的条数--}}
-                    {{--, curr: "{{$currentPage}}"  //当前页--}}
-                    {{--, jump: function (obj, first) {--}}
-                        {{--if (!first) {--}}
-                            {{--window.location.href="/seller/seckill/list?currentPage="+obj.curr;--}}
-                        {{--}--}}
-                    {{--}--}}
-                {{--});--}}
+                laypage.render({
+                    elem: 'page' //注意，这里 是 ID，不用加 # 号
+                    , count: "{{$total}}" //数据总数，从服务端得到
+                    , limit: "{{$pageSize}}"   //每页显示的条数
+                    , curr: "{{$currentPage}}"  //当前页
+                    , jump: function (obj, first) {
+                        if (!first) {
+                            window.location.href="/seller/seckill/list?currentPage="+obj.curr;
+                        }
+                    }
+                });
             });
         }
-        $('#btn_trash').click(function () {
-            let seckill_id = $('#btn_trash').attr('data_id');
-            let currentPage = $('#btn_trash').attr('data_page');
+        $('.btn_trash').click(function () {
+            let seckill_id = $(this).attr('data_id');
+            console.log(seckill_id);
             $.ajax({
-                'url':'/seller/seckill/delete',
-                'data':{
-                    'seckill_id': seckill_id,
-                    'currentPage':currentPage
+                url:'/seller/activity/deletePromoter',
+                data:{
+                    id: seckill_id
                 },
-                'type':'post',
+                type:'post',
                 success:function (res) {
                     if (res.code == 1){
-                        window.location.href="{{url('/seller/seckill/list')}}";
+                        layer.msg(res.msg);
+                        setTimeout(window.location.reload(),2000);
                     } else {
                         layer.msg(res.msg);
                     }
