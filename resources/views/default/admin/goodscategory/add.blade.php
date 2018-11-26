c49fd7a59175acfa9dd0e664de7ce0bd169bde90
DESKTOP-7P97L8T\kery <kery0818@163.com>
2018-11-26 15:37:17 +0800

    no message

--- resources/views/default/admin/goodscategory/add.blade.php
+++ resources/views/default/admin/goodscategory/add.blade.php
@@ -225,7 +225,7 @@
             });
 
             jQuery.validator.addMethod("specialchar", function(value, element) {
-                return this.optional(element) || !/[@'\\"#$%&\^*]/.test(value);
+                return this.optional(element) || !/[@'\\"#$&\^*]/.test(value);
             },("不能包含特殊字符"));
 
             $('#category_info_form').validate({
