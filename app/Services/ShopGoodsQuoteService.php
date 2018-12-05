019e4c3d791f9ba0ef5c65cad360f4c2d2a6b1ea
maomin <traveller_maomin@163.com>
2018-12-05 15:53:48 +0800

    version 16.2

--- app/Services/ShopGoodsQuoteService.php
+++ app/Services/ShopGoodsQuoteService.php
@@ -139,12 +139,15 @@ class ShopGoodsQuoteService
     public static function getShopGoodsQuoteById($id)
     {
         $info = ShopGoodsQuoteRepo::getInfo($id);
+        if(empty($info)){
+            return false;
+        }
         $goods_detail = GoodsRepo::getInfo($info['goods_id']);
         $info['goods_desc'] = $goods_detail['goods_desc'];//商品详情
         $info['brand_name'] = $goods_detail['brand_name'];//品牌
         $info['goods_sn'] = $goods_detail['goods_sn'];//编号
         $info['unit_name'] = $goods_detail['unit_name']; //单位
-        $info['packing_spec'] = $goods_detail['packing_spec'];//包装规格
+        $info['packing_spec'] = $goods_detail['packing_spec'].$goods_detail['unit_name']."/".$goods_detail['packing_unit'];//包装规格
         $info['packing_unit'] = $goods_detail['packing_unit'];//包装单位
         $arr = explode(";", $goods_detail['goods_attr']);
         $info['goods_attr'] = $arr;
