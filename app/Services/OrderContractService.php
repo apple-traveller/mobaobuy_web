cae4161faa1ef28d825b5f8b4b67d0f773585754
CVS7BMVGCW4ID2M\Administrator <625198492@qq.com>
2018-12-06 20:44:02 +0800

    no message

--- app/Services/OrderContractService.php
+++ app/Services/OrderContractService.php
@@ -1,26 +1,6 @@
 <?php
 namespace App\Services;
-use App\Repositories\OrderContractRepo;
 class OrderContractService
 {
     use CommonService;
-
-    public static function getList($order, $condition)
-    {
-        return OrderContractRepo::getList($order, $condition);
-    }
-
-    public static function getListBySearch($pager, $condition)
-    {
-        $res = OrderContractRepo::getListBySearch($pager, $condition);
-        if($res['total'] > 0){
-            foreach ($res['list'] as $k=>$v){
-                #获取订单编号
-//                $order_info =
-                #获取来源用户信息
-            }
-        }
-
-        return $res;
-    }
 }
