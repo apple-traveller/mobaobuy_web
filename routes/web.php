9f98fed9eee7f295c4c32f00457b2d45e2820e03
CVS7BMVGCW4ID2M\Administrator <625198492@qq.com>
2018-12-06 19:17:44 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -208,6 +208,8 @@ Route::group(['namespace'=>'Admin', 'prefix'=>'admin'],function() {
         Route::post('/orderinfo/delivery/modifyShippingBillno', 'OrderInfoController@modifyShippingBillno');//修改快递单号
         Route::post('/orderinfo/delivery/modifyDeliveryStatus', 'OrderInfoController@modifyDeliveryStatus');//修改发货状态
 
+        Route::post('/contract/list', 'OrderInfoController@modifyDeliveryStatus');//合同列表
+
         Route::any('/seckill/list', 'SeckillController@getList');//秒杀活动列表
         Route::post('/seckill/change/status', 'SeckillController@status');//修改秒杀活动启用状态
         Route::get('/seckill/detail', 'SeckillController@detail');//秒杀活动商品详情
