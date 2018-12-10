c5b8913d2f813269b3b6d02b26a01051b137f3eb
YL <1195455487@qq.com>
2018-12-10 15:19:56 +0800

    no message

--- app/Http/Controllers/Seller/LogisticsController.php
+++ app/Http/Controllers/Seller/LogisticsController.php
@@ -8,8 +8,36 @@
 namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\Controller;
+use App\Services\LogisticsService;
+use Illuminate\Http\Request;
 
 class LogisticsController extends Controller
 {
-    
+    public function listView(Request $request)
+    {
+        $currentPage = $request->input('currentPage',1);
+        $shop_id = session('_seller_id')['shop_id'];
+        $shipping_billno = $request->input('shipping_billno','');
+        $order_id = $request->input('order_id','');
+        $condition['is_delete']= 0;
+        $condition['shop_id']= $shop_id;
+        if (!empty($shipping_billno)){
+            $condition['shipping_billno'] = $shipping_billno;
+        }
+
+        if (!empty($order_id)){
+            $condition['order_id'] = $order_id;
+        }
+        $pageSize =5;
+        $list = LogisticsService::getListWithPage(['pageSize'=>$pageSize,'page'=>$currentPage],$condition);
+        return $this->display('seller.shopSalesman.list',[
+            'total'=>$list['total'],
+            'list'=>$list['list'],
+            'currentPage'=>$currentPage,
+            'shipping_billno' => $shipping_billno,
+            'order_id' => $order_id,
+            'pageSize'=>$pageSize
+        ]);
+    }
+
 }
--- app/Services/LogisticsService.php
+++ app/Services/LogisticsService.php
@@ -1,8 +1,31 @@
 <?php
 namespace App\Services;
 
+use App\Repositories\LogisticsRepo;
+
 class LogisticsService
 {
     use CommonService;
+
+    /**
+     * 分页列表
+     * @param $pager
+     * @param $condition
+     * @return mixed
+     */
+    public static function getListWithPage($pager,$condition)
+    {
+        return LogisticsRepo::getListBySearch($pager,$condition);
+    }
+
+    /**
+     * 获取详情
+     * @param $where
+     * @return array
+     */
+    public static function getDetail($where)
+    {
+        return LogisticsRepo::getInfoByFields($where);
+    }
 
 }
