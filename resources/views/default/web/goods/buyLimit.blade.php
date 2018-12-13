29133da3d73bce972458d35a610458ca7d569c5a
yepeng <329710434@qq.com>
2018-12-13 16:10:54 +0800

    no message

--- resources/views/default/web/goods/buyLimit.blade.php
+++ resources/views/default/web/goods/buyLimit.blade.php
@@ -66,16 +66,16 @@
 				<li>
 					<div class="ms_list_center">
 						<div class="ovh">
-							<h1 class="fs24 fl ovhwp" style="height: 36px;width:190px;">{{$v['goods_name']}}</h1><span class="fr pt10 ovh di"><font class="orange">{{$v['click_count']}}</font>次浏览</span>
+							<h1 class="fs18 fl ovhwp" style="height: 36px;width:250px;">{{$v['goods_name']}}</h1><span class=" ovh di"><font class="orange">{{$v['click_count']}}</font>次浏览</span>
 						</div>
-						<div class="ovh mt30">
+						<div class="ovh mt10">
 							<div class="mx_addr fl" style="width: 117px;">{{$v['shop_name']}}</div><div class="mx_addr fl ml15" style="width: 117px;">{{$v['num']}}kg</div>
 						</div>
-						<div class="ovh mt20 ">
+						<div class="ovh mt10 ">
 
 							<!-- <div class="mx_progress"><div class="mx_progress_com" ></div></div><span class="fl fs16 ml10 gray">可售{{$v['available_quantity']}}kg</span> -->
 
-							<div class="mx_progress">
+							<div class="mx_progress" style="width:250px;">
 								@if($v['available_quantity'] <= 0)
 									<div class="mx_progress_com" style="width: 100%"></div>
 								@elseif($v['num'] <= 0)
