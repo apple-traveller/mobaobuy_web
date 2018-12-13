fc7d4ee704426edb80a5204ecb9eb447e32eb757
CVS7BMVGCW4ID2M\Administrator <625198492@qq.com>
2018-12-13 17:27:01 +0800

    no message

--- app/Http/Middleware/WebClosed.php
+++ app/Http/Middleware/WebClosed.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Middleware;
 use App\Http\Controllers\Controller;
+use App\Repositories\FirmUserRepo;
 use App\Repositories\UserRealRepo;
+use App\Repositories\UserRepo;
+use App\Services\FirmUserService;
 use App\Services\UserService;
 use Closure;
 use Illuminate\Support\Facades\View;
@@ -20,36 +23,30 @@ class WebClosed extends Controller
             print_r(getConfig('close_comment'));
             die();
         }
-
         if(!empty(session('_web_user_id'))){
             //缓存用户的基本信息
 
-            if(session()->has('_web_user')){
-                if(session('_web_user.is_real') == 2){
-                    $user_real_count = UserRealRepo::getTotalCount(['user_id'=>session('_web_user_id'),'review_status'=>1]);
-                    if($user_real_count > 0){
+            if(session()->has('_web_user')) {
+                if (session('_web_user.is_real') == 2) {
+                    $user_real_count = UserRealRepo::getTotalCount(['user_id' => session('_web_user_id'), 'review_status' => 1]);
+                    if ($user_real_count > 0) {
                         session()->forget('_web_user');
                         session()->forget('_curr_deputy_user');
                     }
                 }
-                if(session()->has('_curr_deputy_user') && session('_curr_deputy_user.is_self') != 1) {
+                if (session()->has('_curr_deputy_user') && session('_curr_deputy_user.is_self') != 1) {
                     $user_info = UserService::getInfo(session('_web_user_id'));
                     //用户不切换生效权限,is_logout存的是企业的id
                     if ($user_info['is_logout'] > 0 && $user_info['is_logout'] == session('_curr_deputy_user.firm_id')) {
-//                        if ($user_info['is_firm'] == 0 && session('_curr_deputy_user')['is_self'] == 0) {
-                            //获取用户所代表的公司
-                            //firms是firmuser表信息 加企业名称和addressid
-//                            $firms = UserService::getUserFirms(session('_web_user_id'));
-//                            foreach ($firms as $firm) {
-                                    //修改代表信息
-                                    $firm['is_self'] = 0;
-                                    $firm['is_firm'] = 1;
-                                    $firm['name'] = $firm['firm_name'];
-                                    session()->put('_curr_deputy_user', $firm);
-
-                                    UserService::modify(session('_web_user_id'), ['is_logout' => 0]);
-//                            }
-//                        }
+                        $firm = FirmUserRepo::getInfoByFields(['firm_id'=>session('_curr_deputy_user.firm_id'),'user_id'=>session('_web_user_id')]);
+                        $firm_info = UserRepo::getInfo($firm['firm_id']);
+                        //修改代表信息
+                        $firm['is_self'] = 0;
+                        $firm['is_firm'] = 1;
+                        $firm['name'] = $firm_info['nick_name'];
+                        $firm['address_id'] = $firm_info['address_id'];
+                        session()->put('_curr_deputy_user', $firm);
+                        UserService::modify(session('_web_user_id'), ['is_logout' => 0]);
                     }
                 }
             }
