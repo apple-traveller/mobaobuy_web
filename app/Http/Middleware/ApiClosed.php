818fff51a848d5e04bcdfecc80b549d39706d02d
maomin <traveller_maomin@163.com>
2018-12-13 18:50:59 +0800

    version 21.8

--- app/Http/Middleware/ApiClosed.php
+++ app/Http/Middleware/ApiClosed.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Http\Controllers\Api\ApiController;
 use App\Services\UserService;
 use App\Services\UserRealService;
+use App\Services\FirmUserService;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -18,12 +19,12 @@ class ApiClosed extends ApiController
         if(getConfig('shop_closed') == '1'){
             return $this->error('网站已关闭');
         }
+
         $uuid = $request->input('token');
         $user_id = Cache::get($uuid, 0);
-        if($user_id!=0){
+        if($user_id!=0 && empty($user_id)){
             //缓存用户的基本信息
             if(Cache::has('_api_user_'.$user_id)) {
-
                 if(Cache::get('_api_user_'.$user_id)['is_real'] == 2){
                     $user_real_count = UserRealService::getTotalCount(['user_id'=>$user_id,'review_status'=>1]);
                     if($user_real_count > 0){
@@ -38,12 +39,14 @@ class ApiClosed extends ApiController
                     $user_info = UserService::getInfo($user_id);
                     //用户不切换生效权限,is_logout存的是企业的id
                     if ($user_info['is_logout'] > 0 && $user_info['is_logout'] == $deputy_user['firm_id']) {
+                        $firm = FirmUserService::getInfoByCondition(['firm_id'=>$deputy_user['firm_id'],'user_id'=>$user_id]);
+                        $firm_info = UserService::getInfo($firm['firm_id']);
                         $firm['is_self'] = 0;
                         $firm['is_firm'] = 1;
-                        $firm['name'] = $firm['firm_name'];
+                        $firm['name'] = $firm_info['nick_name'];
+                        $firm['address_id'] = $firm_info['address_id'];
                         Cache::put('_api_deputy_user_'.$user_id,$firm,60*24*1);
                         UserService::modify($user_id, ['is_logout' => 0]);
-
                     }
                 }
             }
