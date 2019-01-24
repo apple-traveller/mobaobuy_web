4466b9169dc54d1ace41c2528f2d55b92e9e4b08
maomin <traveller_maomin@163.com>
2019-01-24 09:52:22 +0800

    version 30.8

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -189,6 +189,27 @@ class UserService
         return $info['id'];
     }
 
+    //用户登录
+    public static function loginValidate_mcode($username,$other_params)
+    {
+        //查用户表
+        $info = UserRepo::getInfoByFields(['user_name'=>$username]);
+        if(empty($info)){
+            self::throwBizError('用户名不正确！');
+        }
+        if ($info['is_freeze']) {
+            self::throwBizError('该用户已经被冻结！');
+        }
+        if (!$info['is_validated']) {
+            self::throwBizError('账号需待审核通过后才可登录！');
+        }
+        UserRepo::modify($info['id'],['is_logout'=>0]);
+        unset($info['password']);
+        //登录成功后事件
+        createEvent('webUserLogin', ['user_id'=>$info['id'], 'client_ip'=>$other_params['ip']]);
+        return $info['id'];
+    }
+
 
     //修改密码
     public static function userUpdatePwd($id, $data){
